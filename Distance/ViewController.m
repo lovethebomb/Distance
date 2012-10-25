d934b3bcc7f1a9fb5cf330e7b177e22da322f286
Lucas <lucas@lovethebomb.eu>
2012-10-25 09:45:44 +0200

    Initial Commit

---
+++ Distance/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Distance
+//
+//  Created by HEYMES Lucas on 25/10/12.
+//  Copyright (c) 2012 Heymès Lucas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
